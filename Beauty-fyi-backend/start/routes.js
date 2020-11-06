cf39eacf0cd9c181d9564b8641283e0579cea502
jxmyster <57409773+jxmyster@users.noreply.github.com>
2020-11-06 22:49:38 +0000

    routes

--- Beauty-fyi-backend/start/routes.js
+++ Beauty-fyi-backend/start/routes.js
@@ -19,12 +19,12 @@ const Route = use('Route')
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
-
-Route.get('register', 'Auth/RegisterController.showRegisterForm')
-Route.post('register', 'Auth/RegisterController.register').as('register')
-Route.get('register/confirm/:token', 'Auth/RegisterController.confirmEmail')
-Route.get('login', 'Auth/LoginController.showLoginForm')
-Route.post('login', 'Auth/LoginController.login').as('login')
-Route.post('landing', 'Landing/LandingController.getCSRF').as('getCSRF')
-
+Route.group(() => {
+  Route.get('register', 'Auth/RegisterController.showRegisterForm')
+  Route.post('register', 'Auth/RegisterController.register').as('register')
+  Route.get('register/confirm/:token', 'Auth/RegisterController.confirmEmail')
+  Route.get('login', 'Auth/LoginController.showLoginForm')
+  Route.post('login', 'Auth/LoginController.login').as('login')
+  Route.post('landing', 'Landing/LandingController.getCSRF').as('getCSRF')
+}).middleware(['guest'])
 //Route.post('landing', 'Landing/LandingController.getCSRF').as('getCSRF').middleware(['auth'])
